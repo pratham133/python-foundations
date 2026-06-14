7365ddba2d18e09369b8bdc13300c1ae9f37cc63
Pratham Pasi <prathampasi124@gmail.com>
2026-06-14 14:10:55 +0530

    Day 3: Added datatypes.py and updated README

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+# Different data types
+
+# String (text)
+name = "Pratham Pasi"
+
+# Integer (whole number)
+age = 22
+
+# Float (decimal number)
+height = 5.6
+
+# Show output
+print(name)
+print(age)
+print(height)
