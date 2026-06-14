5568f1ab4f2bddbe2d3b4c508f0b8c332aeb883b
Pratham Pasi <prathampasi124@gmail.com>
2026-06-14 13:40:13 +0530

    Day 2: Added variables.py

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Program 1 (Variables)
+
+# Store personal details
+name = "Pratham Pasi"
+age = 22
+city = "Mumbai"
+
+# Show output
+print(name)
+print(age)
+print(city)
+
+
+# Program 2 (Same Program with Slightly Better Structure)
+
+# User details program
+
+# Get user input
+name = input("Enter your name: ")
+age = input("Enter your age:   ")
+city = input("Enter your city: ")
+
+# Display output
+print("My name is:", name)
+print("My age is:", age)
+print("I live in:", city)
