262a227709aed1e8d142517479bb79c917fd7e00
Pratham Pasi <prathampasi124@gmail.com>
2026-07-06 14:35:03 +0530

    Day 25: Mastered Python Exception Handling an Fixed the code comment

--- Day-25/exception_basics.py
+++ Day-25/exception_basics.py
@@ -1,148 +1,148 @@
-# # # ==========================================
-# # # Program 1 (Without Exception Handling)
-# # # ==========================================
+# ==========================================
+# Program 1 (Without Exception Handling)
+# ==========================================
 
-# # number = int(input("Enter a number: "))
+number = int(input("Enter a number: "))
 
-# # print(f"You entered: {number}")
-
-
-# # # ==========================================
-# # # Program 2 (Handling ValueError)
-# # # ==========================================
-
-# # try:
-# #     number = int(input("Enter a number: "))
-# #     print(f"You entered: {number}")
-
-# # except ValueError:
-# #     print("Invalid input! Please enter a valid integer/number.")
+print(f"You entered: {number}")
 
 
-# # ==========================================
-# # Program 3 (Handling Multiple Exceptions)
-# # ==========================================
+# ==========================================
+# Program 2 (Handling ValueError)
+# ==========================================
 
-# try:
-#     number = int(input("Enter a number: "))
-#     result = 10/number
+try:
+    number = int(input("Enter a number: "))
+    print(f"You entered: {number}")
 
-#     print(f"Result: {result}")
-
-# except ValueError:
-#     print("Invalid input!, Enter a valid integer/number.")
-
-# except ZeroDivisionError:
-#     print("You cannot divide by zero.")
+except ValueError:
+    print("Invalid input! Please enter a valid integer/number.")
 
 
-# # ==========================================
-# # Program 4 (Handling Different Exceptions)
-# # ==========================================
+# ==========================================
+# Program 3 (Handling Multiple Exceptions)
+# ==========================================
 
-# try:
-#     numbers = [10, 20, 30]
+try:
+    number = int(input("Enter a number: "))
+    result = 10/number
 
-#     index = int(input("\nEnter index: "))
+    print(f"Result: {result}")
 
-#     print(numbers[index])
+except ValueError:
+    print("Invalid input!, Enter a valid integer/number.")
 
-# except ValueError:
-#     print("Please enter a valid integer.")
-
-# except IndexError:
-#     print("Index out of range.")
-
-
-# # ==========================================
-# # Program 5 (Generic Exception)
-# # ==========================================
-
-# try:
-#     number = int(input("Enter a number: "))
-#     result = 100 // number
-
-#     print(f"Result: {result}")
-
-# except Exception as error:
-#     print(f"An error occurred: {error}")
+except ZeroDivisionError:
+    print("You cannot divide by zero.")
 
 
-# # ==========================================
-# # Program 6 (Specific vs Generic Exception)
-# # ==========================================
+# ==========================================
+# Program 4 (Handling Different Exceptions)
+# ==========================================
 
-# try:
-#     number = int(input("\nEnter a number: "))
-#     result = 100 / number
+try:
+    numbers = [10, 20, 30]
 
-#     print(f"Result: {result}")
+    index = int(input("\nEnter index: "))
 
-# except ValueError:
-#     print("Invalid input! Only integer/number is allowed.")
+    print(numbers[index])
 
-# except ZeroDivisionError:
-#     print("You cannot divide by zero(0)")
+except ValueError:
+    print("Please enter a valid integer.")
 
-# except Exception as error:
-#     print(f"💀 Error occurred: {error}")
+except IndexError:
+    print("Index out of range.")
 
 
-# # =============================================
-# # Program 7 (Multiple Exceptions in One Block)
-# # =============================================
+# ==========================================
+# Program 5 (Generic Exception)
+# ==========================================
 
-# try:
-#     number = int(input("Enter a number: "))
-#     result= 500 // number
+try:
+    number = int(input("Enter a number: "))
+    result = 100 // number
 
-#     print(f"Result: {result}")
+    print(f"Result: {result}")
 
-# except(ValueError, ZeroDivisionError):
-#     print("Invalid input! Enter a non-zero integer.")
+except Exception as error:
+    print(f"An error occurred: {error}")
 
 
-# # ==========================================
-# # Program 8 (Using else)
-# # ==========================================
+# ==========================================
+# Program 6 (Specific vs Generic Exception)
+# ==========================================
 
-# try:
-#     number = int(input("Enter a number: "))
+try:
+    number = int(input("\nEnter a number: "))
+    result = 100 / number
+
+    print(f"Result: {result}")
+
+except ValueError:
+    print("Invalid input! Only integer/number is allowed.")
+
+except ZeroDivisionError:
+    print("You cannot divide by zero(0)")
+
+except Exception as error:
+    print(f"💀 Error occurred: {error}")
+
+
+# =============================================
+# Program 7 (Multiple Exceptions in One Block)
+# =============================================
+
+try:
+    number = int(input("Enter a number: "))
+    result= 500 // number
+
+    print(f"Result: {result}")
+
+except(ValueError, ZeroDivisionError):
+    print("Invalid input! Enter a non-zero integer.")
+
+
+# ==========================================
+# Program 8 (Using else)
+# ==========================================
+
+try:
+    number = int(input("Enter a number: "))
    
-# except ValueError:
-#     print("Invalid input!")
+except ValueError:
+    print("Invalid input!")
 
-# else:
-#     print(f"You entered: {number}")
-
-
-# # ==========================================
-# # Program 9 (Using finally)
-# # ==========================================
-
-# try: 
-#     number = int(input("Enter a number: "))
-
-# except ValueError:
-#     print("Invalid input!")
-
-# else:
-#     print(f"You entered: {number}")
-
-# finally:
-#     print("Program execution completed.")
+else:
+    print(f"You entered: {number}")
 
 
-# # ==========================================
-# # Program 10 (Using raise)
-# # ==========================================
+# ==========================================
+# Program 9 (Using finally)
+# ==========================================
 
-# age = int(input("\nEnter your age: "))
+try: 
+    number = int(input("Enter a number: "))
 
-# if age < 18:
-#     raise ValueError("You must be at least 18 years old.")
+except ValueError:
+    print("Invalid input!")
 
-# print("Your are eligible.")
+else:
+    print(f"You entered: {number}")
+
+finally:
+    print("Program execution completed.")
+
+
+# ==========================================
+# Program 10 (Using raise)
+# ==========================================
+
+age = int(input("\nEnter your age: "))
+
+if age < 18:
+    raise ValueError("You must be at least 18 years old.")
+
+print("Your are eligible.")
 
 
 # ==========================================
